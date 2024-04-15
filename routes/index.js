bbe0b57b1b207a2e063e40e3de0a6ffa673129a7
cypherHades <xjadezbix@gmx.com>
2024-04-15 16:47:54 -0500

    feat(routes): create index route

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+	{
+		text: "Hi there!",
+		user: "Amando",
+		added: new Date(),
+	},
+	{
+		text: "Hello, world!",
+		user: "Charles",
+		added: new Date(),
+	},
+];
+
+/* GET home page. */
+router.get("/", (req, res, next) => {
+	res.render("index", {
+		title: "Message Board",
+		messages: messages,
+	});
+});
+
+export default router;
