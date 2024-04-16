ff3c61a976f3d6b9c957da9cfd5fb11e192a07d9
cypherHades <xjadezbix@gmx.com>
2024-04-16 11:54:59 -0500

    feat(routes): implement create new post on `/new`

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,17 @@
+import Controller from "../controllers/Controller.js";
 import express from "express";
 
 const router = express.Router();
 
+const message = {
+	user: "",
+	text: "",
+};
+
 router.get("/", (req, res, next) => {
-	res.render("new", { title: "Add a new post" });
+	res.render("new", { title: "Add a new post", message });
 });
+
+router.post("/", new Controller().createNewPost);
 
 export default router;
