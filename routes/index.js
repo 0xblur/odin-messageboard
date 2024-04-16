b8dad5112ffae6a90524d57396b7886834c8a73a
cypherHades <xjadezbix@gmx.com>
2024-04-16 15:52:39 -0500

    feat(routes): load DB from index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 import express from "express";
-import mockDB from "../models/Posts.js";
 const router = express.Router();
 
 /* GET home page. */
+const mockDB = [
+	{
+		text: "Hi there!",
+		user: "Amando",
+		added: new Date(),
+	},
+	{
+		text: "Hello, world!",
+		user: "Charles",
+		added: new Date(),
+	},
+];
+
 router.get("/", (req, res, next) => {
 	res.render("index", {
 		title: "Message Board",
